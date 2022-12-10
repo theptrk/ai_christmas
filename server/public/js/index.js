ce3ddc797fcb55a397887cb55679ccd18af36c94
Patrick Tran <patrick.tran06@gmail.com>
2022-12-10 14:27:24 -0800

    query our /openai endpoint instead of the API on the client

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -1,48 +1,16 @@
-let songSubject;
+document
+  .getElementById("form_id")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+    document.getElementById(
+      "christmas_song_container"
+    ).innerHTML = `<p>Elves writing song...</p>`;
 
-let openai_key = "sk-J6eJnpgZkRbHLoseB16zTfT3BlbkFJVO7uJIAkiUloYgCTs2Zf";
+    let songSubject = document.getElementById("song_subject").value;
+    let res = await fetch(`/openai?s=${encodeURIComponent(songSubject)}`);
+    let { song } = await res.json();
 
-document.getElementById("form_id").addEventListener("submit", function (e) {
-  e.preventDefault();
-  songSubject = document.getElementById("song_subject").value;
-  document.getElementById(
-    "christmas_song_container"
-  ).innerHTML = `<p>Elves writing song...</p>`;
-  let data = {
-    model: "text-davinci-003",
-    prompt: `Write a Christmas song about "${songSubject}"`,
-    temperature: 0,
-    max_tokens: 400,
-  };
-
-  // https://beta.openai.com/docs/api-reference/making-requests
-  fetch("https://api.openai.com/v1/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${openai_key}`,
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      let top_choice = data.choices[0].text;
-      let top_choice_html = top_choice.split("\n").join("<br />");
-      document.getElementById(
-        "christmas_song_container"
-      ).innerHTML = `${top_choice_html}`;
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-});
-
-document.getElementById("secret_form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  let secret = document.getElementById("secret_input").value;
-  for (var i = 0; i < secret.length; i++) {
-    let index = Number(secret[i]);
-    openai_key = openai_key.slice(0, index) + openai_key.slice(index + 1);
-  }
-  document.getElementById("secret_form").style.display = "none";
-});
+    setTimeout(() => {
+      document.getElementById("christmas_song_container").innerHTML = song;
+    }, 700);
+  });
